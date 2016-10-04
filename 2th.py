255b77e65fe2a66ed22f690ad740aa77b9c7fc94
skku-0001 <qkfkadlemf@naver.com>
2016-10-04 19:34:25 +0900

    2th

---
+++ 2th.py
@@ -0,0 +1,21 @@
+# a = 'skku'
+# b = a.replace('s', 'g')
+# c = 3
+
+# print('I replaced {} to {}', format(a,b))
+
+print(1/3)
+print(1//3)
+print(3**2)
+
+r="fadfa\n"
+print(r)
+
+
+r=r"dfakam\n"
+
+print(r)
+
+
+
+
